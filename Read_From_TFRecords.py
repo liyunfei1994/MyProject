78bccf1b097cb0418b36c3abc9c2516420928298
Michael Lee <38845930+liyunfei1994@users.noreply.github.com>
2018-11-15 15:28:28 +0800

    Update Read_From_TFRecords.py

--- Read_From_TFRecords.py
+++ Read_From_TFRecords.py
@@ -1,11 +1,16 @@
 import tensorflow as tf
 
+num_cases = 10
+buffer_size = 10000
+batch_size = 2
+num_epochs = 2
+
 filename = ["train.csv.tfrecords"]
 Dataset = tf.data.TFRecordDataset(filename)
 """原有的Dataset中的数据类型是tf.string，需要进行解析才能转成Tensor"""
 """<TFRecordDataset shapes: (), types: tf.string>"""
 """Cast string to float is not supported"""
-print(Dataset)
+# print(Dataset)
 def parser(record):
     features = tf.parse_single_example(
         record,
@@ -16,25 +21,23 @@ def parser(record):
     )
     """{'features': <tf.Tensor 'ParseSingleExample/ParseSingleExample:0' shape=(?,) dtype=float32>, """
     """'label': <tf.Tensor 'ParseSingleExample/ParseSingleExample:1' shape=(?,) dtype=float32>}"""
-    print("features-->",features)
+    # print("features-->",features)
     """解析之后，features 是一个字典"""
     """Type--> <class 'dict'>"""
-    print("Type-->", type(features))
+    # print("Type-->", type(features))
     """此时data的dtype就是tensor"""
     """features['features']--> <class 'tensorflow.python.framework.ops.Tensor'>"""
-    print("features['features']-->",type(features['features']))
+    # print("features['features']-->",type(features['features']))
     data = tf.cast(features['features'], tf.float32)
     """type(data)--> <class 'tensorflow.python.framework.ops.Tensor'>"""
     """data 是个Tensor"""
-    print("type(data)-->",type(data))
+    # print("type(data)-->",type(data))
     label = tf.cast(features['label'], tf.float32)
 
 
     return data, label
 
-"""<MapDataset shapes: ((), ()), types: (tf.float32, tf.float32)>"""
-Dataset = Dataset.map(parser)
-print(Dataset)
+Dataset = Dataset.map(parser).shuffle(buffer_size=buffer_size).batch(batch_size=batch_size).repeat(num_epochs)
 
 iterator = Dataset.make_initializable_iterator()
 
@@ -44,7 +47,7 @@ next_element = iterator.get_next()
 
 with tf.Session() as sess:
     sess.run(iterator.initializer)
-    for i in range(10):
+    for i in range(num_cases):
         data, label = sess.run(next_element)
         """type(data)--> numpy.ndarray"""
-        print(data)
+        print(data.shape)
