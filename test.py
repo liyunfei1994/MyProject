434973870849d042058cdc3cc29b9c44908c6c50
Michael Lee <38845930+liyunfei1994@users.noreply.github.com>
2018-12-05 18:48:02 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,40 +1,49 @@
 import tensorflow as tf
 import Network
-from Read_From_TFRecords_test import iterator
-from Read_From_TFRecords_test import next_element
+from Read_From_TFRecords_test import test_iterator
+from Read_From_TFRecords_test import test_data_batch
+from Read_From_TFRecords_test import test_label_batch
 import time
+import pandas as pd
+import matplotlib.pyplot as plt
 
 
-MOVING_AVERAGE_DECAY = 0.999
 log_dir = 'E:/logs'
-feature, labels = next_element
-logits = Network.inference(input_tensor=feature, train=False, regularizer=None)
-test_loss = tf.reduce_mean(tf.square(logits - labels))
-# variable_averages = tf.train.ExponentialMovingAverage(MOVING_AVERAGE_DECAY)
-# variables_to_restore = variable_averages.variables_to_restore()
+"""测试时不关注正则化损失的值，将用于计算正则化损失的函数设置为None"""
+logits = Network.inference(input_tensor=test_data_batch, train=False, regularizer=None)
+test_loss = tf.reduce_mean(tf.square(logits - test_label_batch))
 saver = tf.train.Saver()
-
+test_loss_vec = []
 
 while True:
     with tf.Session() as sess:
-        # sess.run(tf.global_variables_initializer())
+
         """生成器的初始化是一定要的"""
-        sess.run(iterator.initializer)
+        sess.run(test_iterator.initializer)
         print("Reading checkpoints...")
         ckpt = tf.train.get_checkpoint_state(log_dir)
 
-        # print(ckpt)
-        # E:/logs\model.ckpt-31000
-        # print(ckpt.model_checkpoint_path)
         if ckpt and ckpt.model_checkpoint_path:
+            """加载模型"""
             saver.restore(sess=sess, save_path=ckpt.model_checkpoint_path)
 
-            """取出来的是个str"""
+            """通过文件名得到模型保存时迭代的轮数，取出来的是个str"""
             global_step = ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1]
             print("Loading success, global step is %s" % global_step)
+            loss = sess.run(test_loss)
+            test_loss_vec.append(loss)
+            print("Test loss is %.6f" % loss)
+            test = pd.Series(data=test_loss_vec)
+            test.to_csv('E:/test.csv', mode='w')
+            if global_step == '20000':
+                plt.plot(test_loss_vec,'r-',linewidth=2, label='test loss')
+                plt.xlabel('Generation')
+                plt.ylabel('Test loss')
+                plt.legend(loc='upper right')
+                plt.show()
+
         else:
             print("No checkpoint file is found")
-        loss = sess.run(test_loss)
-        print("Test loss is %.2f" % loss)
+
     """程序每隔10秒运行一次，每次运行都是读取最新保存的模型"""
     time.sleep(30)
