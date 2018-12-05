cd7e6639c39c6f16d7a3596b469af6363385ae0c
Michael Lee <38845930+liyunfei1994@users.noreply.github.com>
2018-12-05 18:49:43 +0800

    Update Read_From_TFRecords.py

--- Read_From_TFRecords.py
+++ Read_From_TFRecords.py
@@ -1,53 +1,50 @@
 import tensorflow as tf
 
-num_cases = 10
-buffer_size = 10000
-batch_size = 2
-num_epochs = 2
-
+"""num_cases就是有多少个batch"""
+"""1600个训练数据，1600*100 = 160000"""
+"""400个训练数据，400*100 = 40000"""
+"""要记得区分训练集和测试集"""
+num_cases = 20800
+buffer_size = 10000000
+batch_size = 5
+num_epochs = 65
 filename = ["train.csv.tfrecords"]
-Dataset = tf.data.TFRecordDataset(filename)
-"""原有的Dataset中的数据类型是tf.string，需要进行解析才能转成Tensor"""
-"""<TFRecordDataset shapes: (), types: tf.string>"""
-"""Cast string to float is not supported"""
-# print(Dataset)
+
 def parser(record):
     features = tf.parse_single_example(
         record,
-        features = {
+        features={
             "label": tf.FixedLenSequenceFeature([], tf.float32, allow_missing=True),
             "features": tf.FixedLenSequenceFeature([], tf.float32, allow_missing=True)
         }
     )
-    """{'features': <tf.Tensor 'ParseSingleExample/ParseSingleExample:0' shape=(?,) dtype=float32>, """
-    """'label': <tf.Tensor 'ParseSingleExample/ParseSingleExample:1' shape=(?,) dtype=float32>}"""
-    # print("features-->",features)
-    """解析之后，features 是一个字典"""
-    """Type--> <class 'dict'>"""
-    # print("Type-->", type(features))
-    """此时data的dtype就是tensor"""
-    """features['features']--> <class 'tensorflow.python.framework.ops.Tensor'>"""
-    # print("features['features']-->",type(features['features']))
     data = tf.cast(features['features'], tf.float32)
-    """type(data)--> <class 'tensorflow.python.framework.ops.Tensor'>"""
-    """data 是个Tensor"""
-    # print("type(data)-->",type(data))
     label = tf.cast(features['label'], tf.float32)
-
-
     return data, label
 
-Dataset = Dataset.map(parser).shuffle(buffer_size=buffer_size).batch(batch_size=batch_size).repeat(num_epochs)
 
-iterator = Dataset.make_initializable_iterator()
+with tf.name_scope('input_data') as scope:
+    filename = filename
+    Dataset = tf.data.TFRecordDataset(filename)
+    Dataset = Dataset.map(parser).shuffle(buffer_size=buffer_size
+                                          ).batch(batch_size=batch_size).repeat(num_epochs)
+    iterator = Dataset.make_initializable_iterator()
+    # sess = tf.Session()
+    # sess.run(iterator.initializer)
+    # next_element是个元组
+    next_element = iterator.get_next()
 
-next_element = iterator.get_next()
-"""<class 'tuple'>"""
-# print(type(next_element))
-
+"""
+不是把下面的data和label导入train的文件！！！
+而是把next_element导入train的文件
+"""
 with tf.Session() as sess:
     sess.run(iterator.initializer)
     for i in range(num_cases):
         data, label = sess.run(next_element)
+        # print(label.shape)
         """type(data)--> numpy.ndarray"""
-        print(data.shape)
+        # print(data.shape)
+        """shape-->(5,36)"""
+        # print(label.shape)
+        """shape-->(5,5528)"""
