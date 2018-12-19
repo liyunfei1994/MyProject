d076ed52d360b3ac5c23f6c0e27a83aca7baf47a
Michael Lee <38845930+liyunfei1994@users.noreply.github.com>
2018-12-19 15:14:50 +0800

    用Python实现链表的节点类

---
+++ Data Structure/node.py
@@ -0,0 +1,23 @@
+class Node:
+
+    def __init__(self, initdata):
+        self.data = initdata
+        self.next = None
+
+    def getdata(self):
+        return self.data
+
+    def getnext(self):
+        return self.next
+
+    def setdata(self, newdata):
+        self.data = newdata
+
+    def setnext(self, newnext):
+        self.next = newnext
+
+
+temp = Node(10)
+print(temp.data)
+print(temp.next)
+print(temp.getdata())
