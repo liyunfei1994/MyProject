40ffb80733d277fa326a0c66ecbe17af276e946d
Michael Lee <38845930+liyunfei1994@users.noreply.github.com>
2018-12-21 11:32:18 +0800

    Update binarysearch.py

--- Data Structure/binarysearch.py
+++ Data Structure/binarysearch.py
@@ -15,9 +15,9 @@ def binarysearch(alist, item):
             else:
                 first = midpoint + 1
 
+        print("item=%d" % item)
+        print("中点是%d, 值为%d" % (midpoint, alist[midpoint]))
         print("移动之后first=%d, last=%d" % (first, last))
-
-        print("中点是",midpoint)
 
     return found
 
@@ -25,13 +25,17 @@ def binarysearch(alist, item):
 testlist = [1, 2, 8, 15, 19, 23, 34, 40]
 print(binarysearch(testlist, 25))
 
+
 移动之前first=0, last=7
+item=25
+中点是3, 值为15
 移动之后first=4, last=7
-中点是 3
 移动之前first=4, last=7
+item=25
+中点是5, 值为23
 移动之后first=6, last=7
-中点是 5
 移动之前first=6, last=7
+item=25
+中点是6, 值为34
 移动之后first=6, last=5
-中点是 6
 False
