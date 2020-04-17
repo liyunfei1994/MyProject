17bf70c19c16a0aa7dbabb42be0d458d54b8cf9c
Michael Lee <38845930+liyunfei1994@users.noreply.github.com>
2020-04-17 14:00:49 +0800

    cin 连续输入示例

---
+++ Learning_C++/cin.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	int x;
+	float y;
+
+	cout<<"Please input an int number and a float number:"<<endl;
+	cin>>x>>y;
+	cout<<"The int number is x="<<x<<endl;
+	cout<<"The float numver is y="<<y<<endl;
+
+	return 0;
+}
+
+-----------------
+Please input an int number and a float number:
+4 90.1
+The int number is x=4
+The float numver is y=90.1
