7ea037490521c01ba135eaab356dcad1fe250066
Michael Lee <38845930+liyunfei1994@users.noreply.github.com>
2020-10-10 12:08:24 +0800

    Read data from tfrecords file
    input it into the network

---
+++ CNN/train_read_from_tfrecords.py
@@ -0,0 +1,48 @@
+import tensorflow as tf
+from tfrecords_train import IMG_HEIGHT, IMG_WIDTH
+
+buffer_size = 100000
+batch_size = 128
+num_epochs = 900
+filename = ["train.tfrecords"]
+
+
+def parser(record):
+    features = tf.parse_single_example(
+        record,
+        features={
+            "images_raw": tf.FixedLenFeature([], tf.string),
+            "features": tf.FixedLenFeature([10], tf.float32)
+        }
+    )
+
+    # "这里解析的时候图像的数据类型一定要是uint8!!!!!"
+    decoded_image = tf.decode_raw(features['images_raw'], tf.uint8)
+    decoded_image = tf.reshape(decoded_image, [IMG_HEIGHT, IMG_WIDTH])
+    # decoded_images.shape [252, 400]
+    # "但是可以在这里进行数据类型的修改！！！"
+    decoded_image = tf.cast(decoded_image, tf.float32)
+    print("decoded_images.shape", decoded_image.get_shape().as_list())
+
+    feature = features['features']
+    # feature.shape [10]
+    print("feature.shape", feature.get_shape().as_list())
+    return decoded_image, feature
+
+
+with tf.name_scope('train_input_data') as scope:
+    Dataset = tf.data.TFRecordDataset(filename)
+    print("=" * 10)
+    print("batch_size=", batch_size)
+    Dataset = Dataset.map(parser).shuffle(buffer_size=buffer_size).repeat(num_epochs).batch(batch_size=batch_size)
+    train_iterator = Dataset.make_initializable_iterator()
+    train_next_element = train_iterator.get_next()
+
+# with tf.Session() as sess:
+#     sess.run(train_iterator.initializer)
+#     for i in range(2):
+#         images, features = sess.run(train_next_element)
+#         # (64, 252, 400)
+#         print("images.shape",images.shape)
+#         # (64, 10)
+#         print("features.shape", features.shape)
